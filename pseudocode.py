f3ebee00aef33717f13c1a8498539064d7eb7c0f
Alfian Mutakim <136336621+alfianmutaqin01@users.noreply.github.com>
2023-06-20 22:16:26 +0700

    Add files via upload

---
+++ pseudocode.py
@@ -0,0 +1,18 @@
+nilai = float(input("Masukkan nilai Anda: "))
+
+if nilai > 85:
+    print("Grade A")
+elif nilai > 75:
+    print("Grade = B")
+elif nilai > 65:
+    print("Grade = C")
+elif nilai > 55:
+    print("Grade = D")
+else:
+    print("Grade = E")
+    nilai = float(input("Masukkan nilai Anda: "))
+    nilai = float(input("Masukkan nilai Anda: "))
+    nilai = float(input("Masukkan nilai Anda: "))
+    nilai = float(input("Masukkan nilai Anda: "))
+    nilai = float(input("Masukkan nilai Anda: "))
+    nilai = float(input("Masukkan nilai Anda: "))
